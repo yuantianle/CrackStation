f39a7e02c927c8dce2c79cbdae347e13a55b1db6
Tianle Yuan <yuant@oregonstate.edu>
2022-11-20 22:22:41 +0000

    self document and readme

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -2,49 +2,44 @@ import Foundation
 
 public class CrackStation: Decrypter {
 
-    private let Hash: [String : String]
+    private let HashTable: [String : String]
 
     public required init(){
-        self.Hash = try! CrackStation.loadDictionaryFromDisk()
+        self.HashTable = CrackStation.loadDictionaryFromDisk()
     }
 
-    static func loadDictionaryFromDisk() throws -> [String : String] {
-        guard let path = Bundle.module.url(forResource: "hash_data3", withExtension: "json") else { return [:] }
+    static func loadDictionaryFromDisk() -> [String : String] {
+        do{
+            guard let path = Bundle.module.url(forResource: "hash_data3", withExtension: "json") else { return [:] }
 
-        let data = try Data(contentsOf: path)
-        let jsonResult = try JSONSerialization.jsonObject(with: data)
+            let data = try Data(contentsOf: path)
+            let jsonResult = try JSONSerialization.jsonObject(with: data)
 
-        if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
-            return lookupTable
-        } else {
+            if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
+                return lookupTable
+            } else {
+                return [:]
+            }
+        }
+        catch{
             return [:]
         }
     }
 
-    //enum MyError: Error {
-    //    case runtimeError(String)
-    //}
-
+    //Time complexity: O(1)
     public func decrypt(shaHash: String) -> String? {
         //prefilter
         if (shaHash.count != 40 && shaHash.count != 64) {
-        // we are about to actually throw an error:
-            //throw MyError.runtimeError("Illegal length.")
             return nil
         }
         else if (shaHash.rangeOfCharacter(from: CharacterSet.uppercaseLetters) != nil) {
-        // we are about to actually throw an error:
-            //throw MyError.runtimeError("include uppercase.")
             return nil
         }
         else if (shaHash.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted) != nil) {
-        // we are about to actually throw an error:
-            //throw MyError.runtimeError("include illegal char.")
             return nil
         }
         else {
-            //let lookupTable = try? CrackStation.loadDictionaryFromDisk()
-            return Hash[shaHash]
+            return HashTable[shaHash]
         }
     }
 
